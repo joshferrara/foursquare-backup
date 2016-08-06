edf719ceabdef4c0e3a2b7e738e53ac21c09d2ed
Josh Ferrara <joshferrara@gmail.com>
2016-08-05 20:22:54 -0500

    Cleaning up the code I borrowed

--- export.py
+++ export.py
@@ -2,22 +2,18 @@
 import requests
 import json
  
-url_template = 'https://api.foursquare.com/v2/users/self/checkins?limit=250&oauth_token={}&v=20131026&offset={}'
+url = 'https://api.foursquare.com/v2/users/self/checkins?limit=250&oauth_token={}&v=20131026&offset={}'
  
-# If you navigate to https://developer.foursquare.com/docs/explore, Foursquare
-# will generate an OAuth token for you automatically. Cut and paste that token
-# below.
-oauth_token = "YOUR_TOKEN_GOES_HERE"
+# Visit https://developer.foursquare.com/docs/explore to generate an OAuth token. Paste that token below.
+oauth_token = "5QCFUOK2COFUI1P2QTUPPLB2Q3DD5ZU0RTKCRYLWMGKWPGEW&v=20160805"
 offset = 0
 data = []
  
 with open("/tmp/checkins.json", 'w') as f:
-    while True:
-        response = requests.get(url_template.format(oauth_token, offset))
-        if len(response.json()['response']['checkins']['items']) == 0:
-            break
-
-        data.append(response.json())
-        offset += 250
-            
-    f.write(json.dumps(data))
+  while True:
+    response = requests.get(url.format(oauth_token, offset))
+    if len(response.json()['response']['checkins']['items']) == 0:
+      break
+    data.append(response.json())
+    offset += 250
+  f.write(json.dumps(data))
